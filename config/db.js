45f1efe4194fed5ca388bb8e61e51adda8b9bf18
SanikaBhandari07 <156589437+SanikaBhandari07@users.noreply.github.com>
2025-01-03 16:28:05 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/capstone', {
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+module.exports = connectDB;
